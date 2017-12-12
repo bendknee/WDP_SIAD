b3da75a261cf11d135182fc61afe622cfe6a2c9a
Tirta Hema Jaya <tirta.hema.jaya@gmail.com>
2017-12-12 21:35:41 +0700

    hehe

--- apps_status/models.py
+++ apps_status/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps_profile.models import User
 
 # Create your models here.
 
 class Status(models.Model):
+    user = models.ForeignKey(User, default="kosong")
     status = models.CharField(max_length=140)
     created_date = models.DateTimeField(auto_now_add=True)
--- apps_status/views.py
+++ apps_status/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from .forms import Status_Form
 from .models import Status
-
+from apps_profile import models
 # Create your views here.
 
 response = {}
@@ -10,14 +10,19 @@ def index(request):
     response['status_form'] = Status_Form
     response['status'] = Status.objects.all()[::-1]
     html = 'apps_status/status.html'
+    if 'user_login' in request.session:
+        npm = request.session['kode_identitas']          
     return render(request,html,response)
 
 def update_status(request):
     form = Status_Form(request.POST or None)
     if(request.method == 'POST' and form.is_valid()):
         status = request.POST['status']
-        status = Status(status = status)
-        status.save()
+        if 'user_login' in request.session:
+            npm = request.session['kode_identitas']
+            user = User.objects.get(npm=npm)
+            status = Status(status = status, user=user)
+            status.save()
     return HttpResponseRedirect('/mahasiswa/status/')
 
 def delete_status(request):
