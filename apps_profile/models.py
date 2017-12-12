c22386a9d01b525f7e51a25b17c20c86c21c6ae3
Ariel Miki Abraham <arielmkiabraham@gmail.com>
2017-12-12 19:50:24 +0700

    add models

--- apps_profile/models.py
+++ apps_profile/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True, primary_key=True)
+    npm = models.CharField(max_length=10, unique=True)
+    flag_nilai=  models.BooleanField(default=False)
+    name = models.CharField(max_length=27, default='Kosong')
+    expertise = models.ManyToManyField('Expertise',default='Kosong')
+    email = models.EmailField(default='Kosong')
+    linkedin_profile = models.URLField(default='Kosong')
+
+class Expertise(models.Model):
+    expertise = models.CharField(max_length=20)
