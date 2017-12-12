343c2aa881141287b3f1e5daf938f68442bb2e7f
Ariel Miki Abraham <arielmkiabraham@gmail.com>
2017-12-12 20:06:48 +0700

    add templates

--- apps_profile/views.py
+++ apps_profile/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import *
 
 # Create your views here.
-
+response = {}
 def index(request):
-    pass
+    if 'user_login' in request.session:
+        return render(request,'profile/profile.html',response)
+    else:
+        return HttpResponseRedirect(reverse('login:index'))
