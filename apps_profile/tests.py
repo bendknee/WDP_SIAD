621052ead6a484b883669f0a8ef2ce334735001e
Ariel Miki Abraham <arielmkiabraham@gmail.com>
2017-12-13 05:05:18 +0700

    test 80%

--- apps_profile/tests.py
+++ apps_profile/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.http import HttpRequest
+from .models import *
 import environ
 
 #requirements
@@ -22,5 +23,11 @@ class ProfileUnitTest(TestCase):
     def test_profile_edit_is_exist(self):
         self.client.post('/mahasiswa/login/custom_auth/login/', {'username': self.username, 'password': self.password},follow=True)
         npm = self.client.session['kode_identitas']
+        User.objects.create(npm=npm)
         response = self.client.get('/mahasiswa/profile/'+npm+'/edit/',follow=True)
+        response = self.client.post('/mahasiswa/profile/'+npm+'/edit/',{'email':'lalala@gmail.com','name':'lalala','linkedin':'http://lalala.ni/'},follow=True)
         self.assertEqual(response.status_code,200)
+
+    def test_profile_edit_is_failed(self):
+        self.client.post('/mahasiswa/login/custom_auth/login/', {'username': self.username, 'password': self.password},follow=True)
+        response = self.client.get('/mahasiswa/profile/1606123/edit/',follow=True)
