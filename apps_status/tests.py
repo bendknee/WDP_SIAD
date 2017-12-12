5228d673d6c9f9beeed750e7b3f3bccf6b976ef2
Tirta Hema Jaya <tirta.hema.jaya@gmail.com>
2017-12-13 02:58:33 +0700

    adding unit test for status

--- apps_status/tests.py
+++ apps_status/tests.py
@@ -9,6 +9,19 @@ class statusUnitTest(TestCase):
         response = Client().get('/mahasiswa/status/')
         self.assertEqual(response.status_code, 400)
     
+    def test_model_can_create_status(self):
+        new_status = Status.objects.create(status = 'I play dota everyday~')
+        count = Status.objects.all().count()
+        self.assertEqual(count,1)
+
+    def test_post_status_url_is_exist(self):
+        status = 'I pwn U'
+        response_post = Client().post('/status/update_status/', {'status': status})
+        self.assertEqual(response_post.status_code, 404)
+
+    def test_post_status_url_is_using_update_status_func(self):
+        found = resolve('/status/update_status/')
+        self.assertEqual(found.func, update_status)
 
     def test_status_delete_button(self):
         new_activity = Status.objects.create(status='Do something')
