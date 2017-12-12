03aefdafdef283a7472c68af6b32f7715c2196c8
Ariel Miki Abraham <arielmkiabraham@gmail.com>
2017-12-13 04:43:41 +0700

    done done done

--- apps_profile/tests.py
+++ apps_profile/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.http import HttpRequest
+import environ
 
-# Create your tests here.
+#requirements
+env = environ.Env(DEBUG=(bool, False),)
+environ.Env.read_env('.env')
+class ProfileUnitTest(TestCase):
+    def test_profile_url_is_exist(self):
+        response = Client().get('/mahasiswa/profile/',follow=True)
+        self.assertEqual(response.status_code,200)
+
+    def test_profile_login_is_exist(self):
+        response = self.client.post('/mahasiswa/login/custom_auth/login/',{"username":env('SSO_USERNAME'),"password":env('SSO_PASSWORD')},follow=True)
+        self.assertEqual(response.status_code,404)
