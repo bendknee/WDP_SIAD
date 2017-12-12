9c488e5db1e4d31c9ebd3d05989cd8f095fabca0
Tirta Hema Jaya <tirta.hema.jaya@gmail.com>
2017-12-12 22:06:58 +0700

    fixing add status

--- apps_status/views.py
+++ apps_status/views.py
@@ -3,16 +3,28 @@ from django.http import HttpResponseRedirect, HttpResponse
 from .forms import Status_Form
 from .models import Status
 from apps_profile.models import User
+from apps_login.csui_helper import get_data_user
 # Create your views here.
 
 response = {}
 def index(request):
-    response['status_form'] = Status_Form
-    response['status'] = Status.objects.all()[::-1]
-    html = 'apps_status/status.html'
     if 'user_login' in request.session:
-        npm = request.session['kode_identitas']          
-    return render(request,html,response)
+        npm = request.session['kode_identitas']
+        access_token = request.session['access_token']
+        nama = get_data_user(access_token,npm)['nama']
+        response['status_form'] = Status_Form
+        response['list_status'] = Status.objects.all()[::-1]
+        html = 'apps_status/status.html'
+        response['author'] = request.session['user_login']
+        user = User.objects.get(npm=npm)
+        status = Status.objects.filter(user=user)
+        response['total_post'] = status.count()
+        if(status.count()>0):
+            response['name'] = nama
+            response['status'] = status.order_by('-id')[0].status
+        if 'user_login' in request.session:
+            npm = request.session['kode_identitas']          
+        return render(request,html,response)
 
 def update_status(request):
     form = Status_Form(request.POST or None)
