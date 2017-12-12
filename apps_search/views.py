a9464b6dc929c630510f83362daee80cdff9e1ab
Benny William Pardede <bpardedewilliam@gmail.com>
2017-12-12 22:49:33 +0700

    header UDAH KOMPAK

--- apps_search/views.py
+++ apps_search/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
-
+from apps_profile.models import User
+import json
+from apps_status.models import Status
+from apps_login.csui_helper import get_data_user
 # Create your views here.
+response = {}
+
 
 def index(request):
-    pass
+    npm = request.session['kode_identitas']
+    access_token = request.session['access_token']
+    nama = get_data_user(access_token,npm)['nama']
+    response['author'] = request.session['user_login']
+    user = User.objects.get(npm=npm)
+    status = Status.objects.filter(user=user)
+    response['total_post'] = status.count()
+    response['name'] = nama
+    if(status.count()>0):
+        response['status'] = status.order_by('-id')[0].status
+    if 'user_login' in request.session:
+        npm = request.session['kode_identitas']
+    html = 'search/search.html'
+    response['data'] = get_user_data()
+    return render(request, html, response)
+
+
+def get_user_data():
+    users = User.objects.all()
+    data = []
+    for user in users:
+        temp = {}
+        temp['username'] = user.username
+        temp['npm'] = user.npm
+        temp['name'] = user.name
+        temp['email'] = user.email
+        temp['linkedin'] = user.linkedin_profile
+        temp['expertise'] = []
+        exprs = user.expertise.all()
+        for expr in exprs:
+            temp['expertise'].append(expr)
+        data.append(temp)
+    return json.dumps(data)
