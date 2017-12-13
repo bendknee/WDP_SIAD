4010ddeec2c69128212e235a4c6903ff127d7254
Ariel Miki Abraham <arielmkiabraham@gmail.com>
2017-12-13 07:12:21 +0700

    solve header

--- apps_riwayat/views.py
+++ apps_riwayat/views.py
@@ -14,24 +14,19 @@ RIWAYAT_API = 'https://private-e52a5-ppw2017.apiary-mock.com/riwayat'
 
 def index(request):
     if 'user_login' in request.session:
+        access_token = request.session['access_token']
+        npm = request.session['kode_identitas']
+        nama = get_data_user(access_token,npm)['nama']
+        response['author'] = request.session['user_login']
+        user = User.objects.get(npm=npm)
+        status = Status.objects.filter(user=user)
+        response['total_post'] = status.count()
+        response['npm'] = npm
+        response['name'] = nama
+        if(status.count()>0):
+            response['status'] = status.order_by('-id')[0].status
         response['riwayats'] = get_riwayat()
-        npm = request.session['kode_identitas']
-        response['npm'] =  request.session['kode_identitas']
-    html = 'app_riwayat/riwayat.html'
-    return render(request, html, response)
-
-    access_token = request.session['access_token']
-    nama = get_data_user(access_token,npm)['nama']
-    response['author'] = request.session['user_login']
-    user = User.objects.get(npm=npm)
-    status = Status.objects.filter(user=user)
-    response['total_post'] = status.count()
-    response['npm'] = npm
-    response['name'] = nama
-    if(status.count()>0):
-        response['status'] = status.order_by('-id')[0].status
-    if 'user_login' in request.session:
-        npm = request.session['kode_identitas']
+    
         html = 'app_riwayat/riwayat.html'
         return render(request, html, response)
     else:
